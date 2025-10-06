01271d6a1a0df463a9f23713b02e335e7cc69f1d
alsongard <alsongard@gmail.com>
2025-10-06 05:08:17 +0300

    mongoose connection function

---
+++ backend/config/connectDB.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async ()=>{
+    try
+    {
+        mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connecting to DB success`);
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+    }
+}
+
+module.exports  = connectDB;
