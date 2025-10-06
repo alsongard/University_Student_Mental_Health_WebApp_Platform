f3803fbfa1ceb71cb11d50dd2076d525073900b3
alsongard <alsongard@gmail.com>
2025-10-06 05:12:45 +0300

    server start function set, routes added: success

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,57 @@
+// import neccessary modules:functions
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const path = require("node:path");
+const connectDB = require("./config/connectDB");
 
 
-
-
-
-
-
-
-
-
-
-
-
+// create app:express instance
 const app = express();
 
+
+// middleware configuration
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+
+
+// ROUTES
+const authPsychatriastRouter = require("./routes/authPsychatriastRouter");
+const authStudentRouter = require("./routes/authStudentRouter");
+
 
 app.get("/", (req,res)=>{
     res.sendFile("index.html");
 })
 
+app.use("/api/student", authStudentRouter);
+app.use("/api/psychatriast", authPsychatriastRouter);
 
 
-app.listen(5000, ()=>{
-    console.log("Listening on port http://localhost:5000");
-})
+
+
+
+
+const  serverStart  =   async()=>
+{
+    try
+    {
+
+        connectDB();
+        // LISTENING 
+        app.listen(5000, ()=>{
+            console.log("Listening on port http://localhost:5000");
+        });
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:`Error: ${err}`});
+    }
+}
+
+
+serverStart();
