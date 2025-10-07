a0fe9766e91160de8a55e947a6060daab9307f47
alsongard <alsongard@gmail.com>
2025-10-07 11:18:22 +0300

    psychiatrist add session model

---
+++ backend/models/psychiatristSession.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const pyschiatristSession = new mongoose.Schema({
+    psychiatristId: {type:mongoose.Schema.Types.ObjectId, ref:Psychatriast, required:true},
+    date:{type:Date , required:true},
+    startTime: {type:String, required:true},
+    endTime: {type:String, required:true},
+    sessionStatus: {type:String, enum:["Available", "Booked"], required:true}
+})
+
+
+const PsychiatristSession = mongoose.model("PsychiatristSession", pyschiatristSession);
+
+module.exports = PsychiatristSession;
