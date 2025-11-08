349cadcb44edbf733001fc4713e37a0b587931c6
alsongard <alsongard@gmail.com>
2025-11-08 03:08:32 +0200

    studentController: createStudent, getStudent:id done

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,74 @@
+const StudentDetails = require('../models/studentDetails.model');
+const jwt = require("jsonwebtoken");
+const createStudentDetails = async (req, res) => {
+    try 
+    {
+        const { studentName, studentAge, gender, phoneNumber, course, yearOfStudy, address, emergencyContact } = req.body;
+        if (!studentName || !studentAge || !gender || !phoneNumber || !course || !yearOfStudy || !address || !emergencyContact) 
+        {
+            return res.status(400).json({success:false, msg:"Please fill in all fields"});
+        }
+        const tempToken = req.cookies.theToken;
+        console.log('tempToken');
+        console.log(tempToken);
+
+        // const {userObject} = tempToken;
+// tempToken
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTAzNjJhNzhkNGZhM2ExNGE3OGE5ZTMiLCJpYXQiOjE3NjI1MjcwNjEsImV4cCI6MTc2MjUzNDI2MX0.SAjGK9SWGYc9LzoAdUwnnZsQGUcrkxhiYaaojzLl-bA
+// verifiedToken
+// {
+//   userId: '690362a78d4fa3a14a78a9e3',
+//   iat: 1762527061,
+//   exp: 1762534261
+// }
+        const verifiedToken = jwt.verify(tempToken, process.env.JWT_SECRET);
+        console.log('verifiedToken');
+        console.log(verifiedToken);
+
+
+        const new_studentDetails = await StudentDetails.create({
+            studentId: verifiedToken.userId,
+            studentName,
+            studentAge,
+            gender,
+            phoneNumber,
+            course,
+            yearOfStudy,
+            address,
+            emergencyContact
+        });
+
+        if (!new_studentDetails)
+        {
+            return res.status(500).json({success:false, msg:"Error creating student details"});
+        }
+
+        return res.status(201).json({success:true, msg:"Student details created successfully", data:new_studentDetails});
+
+    } 
+    catch (err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:`Server Error: ${err}`});
+    }
+}
+
+const getStudentDetails = async (req, res)=>{
+    try 
+    {
+        const {id} = req.params;
+
+        const studentDetails = await StudentDetails.findOne({studentId: id}).populate({path: 'studentId', select: 'studentAdmissionNum email isAccountVerified'});
+
+        if (!studentDetails) {
+            return res.status(404).json({success:false, msg:"Student details not found"});
+        }
+        return res.status(200).json({success:true, data:studentDetails});
+    } 
+    catch (err) {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:`Server Error: ${err}`});
+    }
+}
+
+module.exports = { createStudentDetails, getStudentDetails };
