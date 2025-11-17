7fe7313dfa49c21d52c48f08a9a73b2dc3018cfe
alsongard <alsongard@gmail.com>
2025-11-17 20:07:50 +0200

    update on message model

--- backend/models/messages.model.js
+++ backend/models/messages.model.js
@@ -3,12 +3,18 @@ const Psychatriast = require("./physchatris.model");
 const Student = require("./user.model");
 
 
-const messagesSchema = new mongoose.Schema({
-    sender:{type:mongoose.Schema.Types.ObjectId, required:true, ref: Student || Psychatriast},
-    receiver: {type:mongoose.Schema.Types.ObjectId, required:true, ref: Student || Psychatriast},
-    message:{type:String}
-}, 
-{
-    timestamps:true
-}
+const messagesSchema = new mongoose.Schema(
+    {
+        senderId:{type:String, required:true},
+        receiverId: {type:String, required:true},
+        message:{type:String, required:true},
+        image: {type:String, default: ""}
+    }, 
+    {
+        timestamps:true
+    }
 );
+
+const Message = mongoose.model("Message", messagesSchema);
+module.exports  = Message;
+
