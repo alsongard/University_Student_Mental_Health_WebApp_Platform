a2c2ce6e4a8a24a79d7b234a2ce91dbe6fdb9720
alsongard <alsongard@gmail.com>
2025-11-28 20:31:54 +0200

    update on univweb app done

--- client/univweb_app/middlware.ts
+++ client/univweb_app/middlware.ts
@@ -1,5 +1,5 @@
-// middleware.ts
-import NextAuth from 'next-auth';
+// middleware.ts 280163
+// import NextAuth from 'next-auth';
 import { authConfig } from '@/auth.config';
 
 export default NextAuth(authConfig).auth;
