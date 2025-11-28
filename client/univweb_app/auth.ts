12e3484b79ba9af154fada7f181af5be3850968c
alsongard <alsongard@gmail.com>
2025-11-28 20:31:36 +0200

    handler set with METHODS GET, POST

--- client/univweb_app/auth.ts
+++ client/univweb_app/auth.ts
@@ -5,27 +5,18 @@ import {z} from 'zod';
 import type {User} from "./app/lib/definitions"
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [
-    Credentials({
-        async authorize(credentials)
-        {
-            const parsedCredentials = z
-                .object({emails:z.email(), password:z.string().min(6)})
-                .safeParse(credentials)
-        }
-    })]
 });
 
 
 
 
-async function getUser(email:String): Promise<User | undefined> {
-    try
-    {
-    }
-    catch(err)
-    {
+// async function getUser(email:String): Promise<User | undefined> {
+//     try
+//     {
+//     }
+//     catch(err)
+//     {
 
-    }
+//     }
 
-}
+// }
