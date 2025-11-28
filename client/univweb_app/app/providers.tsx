ca4e9a20620007cd0fc456c01235dd1037456d53
alsongard <alsongard@gmail.com>
2025-11-28 20:29:34 +0200

    add ThemeProvider to Providers

--- client/univweb_app/app/providers.tsx
+++ client/univweb_app/app/providers.tsx
@@ -1,7 +1,7 @@
 // app/providers.tsx
 'use client';
 import { SessionProvider } from 'next-auth/react';
-
+import { ThemeProvider } from 'next-themes';
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider><ThemeProvider attribute='class' defaultTheme='system' enableSystem>{children}</ThemeProvider></SessionProvider>;
 }
