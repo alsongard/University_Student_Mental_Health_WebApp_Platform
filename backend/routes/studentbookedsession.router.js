48ecf1e4d3ae0ca3b9413fd10df0d31e292e9647
alsongard <alsongard@gmail.com>
2025-11-28 20:26:10 +0200

    update on backend routes

--- backend/routes/studentbookedsession.router.js
+++ backend/routes/studentbookedsession.router.js
@@ -1,7 +1,8 @@
 // GET students booked sessions
 const router = require("express").Router();
-
 const {ViewStudentBookedSessions} = require("../controllers/bookingSessionController");
+
+
 router.get("/getStudentBookedSessions/:studentId", ViewStudentBookedSessions);
 
 const studentBookedSessionRoutes = router;
