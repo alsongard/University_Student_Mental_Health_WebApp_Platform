2c94293f2070d2dbe3559b5b27ab99b0231cf211
alsongard <alsongard@gmail.com>
2025-12-11 14:34:30 +0200

    psychiatristDetails models done

---
+++ backend/models/psychiatristdetail.model.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+
+const PsychiatristDetailSchema = new mongoose.Schema({
+    psychiatristId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'PsychiatristSession',
+        required: true,
+        unique: true
+    },
+    fullName : {
+        type:String,
+        required:true
+    },
+    phoneNumber: {
+        type:String, 
+        required:true
+    },
+    officeLocation: {
+        type:String, 
+        required:true
+    },
+    biography: {
+        type:String,
+        required:true,
+    },
+    specilization: {
+        type:String,
+        required:true,
+    },
+    yearsExperience: {
+        type:Number,
+        required:true
+    },
+    Education:{
+        type:String,
+        required:true
+    },
+    consultationDays: {
+        type:[String],
+        required:true,
+        enum: ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday"]
+    },
+    consultationHours: {
+        type:String,
+        required:true
+    }, 
+    notifications: {
+        email: {type:Boolean, required:true},
+        sms: {type:Boolean, required:true},
+        alerts: {type:Boolean, required:true},
+        feedbackNotif: {type:String, required:true}
+    }
+});
+
+const PsychiatristDetails = mongoose.model("PsychiatristDetail", PsychiatristDetailSchema);
+module.exports = PsychiatristDetails;
+
