7ba040e98915ad4f19f4725d388366280a0302e4
alsongard <alsongard@gmail.com>
2025-12-16 21:07:20 +0200

    update on message model:new fields added

--- backend/models/messages.model.js
+++ backend/models/messages.model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-const Psychatriast = require("./physchatris.model");
-const Student = require("./user.model");
+// const Psychatriast = require("./physchatris.model");
+// const Student = require("./user.model");
 
 
 const messagesSchema = new mongoose.Schema(
     {
         senderId:{type:String, required:true},
         receiverId: {type:String, required:true},
+        receiverName: {type:String, required:true},
+        recieverRole: {type:String, required:true},
+        recieverAvatar: {type:String, default: ""},
         message:{type:String, required:true},
         image: {type:String, default: ""}
     }, 
