bf9081a1c6ac415ba1b25415208a240397525625
alsongard <alsongard@gmail.com>
2025-12-17 23:30:01 +0200

    Routes for retrieveChatPartners List and All psychiatrist

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const {sendMessage, retrieveMessagesBtwn} = require("../controllers/message.controller")
-const messageRouter = express.Router()
+const {sendMessage, retrieveMessagesBtwn, retrieveUserChatPartners, getAllPsychiatrist} = require("../controllers/message.controller")
+const messageRouter = express.Router();
 
 
+// getAllContacts from psychiatrist
+messageRouter.get("/getAllPsychiatrist", getAllPsychiatrist); //sidebar for all Contacts
 messageRouter.post("/sendmessage/:id", sendMessage);
 messageRouter.get("/retrievemessages/:id", retrieveMessagesBtwn);
-
+messageRouter.get("/retrieveUserChatPartners/:id", retrieveUserChatPartners);
+// messageRouter.get("/retrieveUsers/:id", getUsers);/
 module.exports = messageRouter;
