f0af2d405a5b9437d4c2af12d498adb8764d88db
alsongard <alsongard@gmail.com>
2025-12-17 23:29:17 +0200

    retrieveUserChatPartners and GetPsychiatrist For Chat Set

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,7 @@
 const getAuthToken = require("../middleware/getAuthToken");
 const jwt = require("jsonwebtoken");
 const Message = require("../models/messages.model");
+const PsychiatristDetails = require("../models/psychiatristdetail.model");
 
 const sendMessage = async (req, res)=>{
     const receiverId = req.params.id;
@@ -57,7 +58,66 @@ const retrieveMessagesBtwn = async(req, res)=>{
     }
 }
 
-module.exports = {retrieveMessagesBtwn, sendMessage}
+
+const retrieveUserChatPartners = async (req,res)=>
+{
+    try
+    {
+        const userId = req.user.id; // this is the sender
+        // const theReceiver = req.params.id;  // this is the receiver
+
+        const messagesBtwn = await Message.find({
+            $or:
+            [
+                {senderId: userId},
+                {receiverId: userId}
+            ]
+        });
+
+
+        // this works as follows
+        // if the sender is Me: return the receiverId , however if me is not senderId return senderId
+        const chatPartnerIds = [...new Set(messagesBtwn.map((messageItem)=>{
+            messageItem.senderId === userId ? messageItem.receiverId : messageItem.senderId 
+        }))];
+
+        console.log('chatPartnerIds');
+        console.log(chatPartnerIds);
+
+
+        // now fetch the psychiatrist Details
+        const foundPsychiatristDetailsPartners = await PsychiatristDetails.find({psychiatristId: {$in: chatPartnerIds}}).select("fullName specilization")
+        console.log('foundPsychiatristDetailsPartners');
+        console.log(foundPsychiatristDetailsPartners);
+        return res.status(200).json({success:true ,msg:"Found Chat Partners", data:foundPsychiatristDetailsPartners});
+
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:failure, msg:`Error: ${err}`});
+    }
+}
+
+const getAllPsychiatrist = async (req, res)=>
+{
+    try
+    {
+        const foundPsychatriast = await  PsychiatristDetails.find().select("fullName specilization");
+        if (!foundPsychatriast)
+        {
+            return res.status(200).json({success:true, msg:"No Psychiatrist found"});
+        }
+        return res.status(200).json({success:true, msg:"Retrieved Psychiatrist", data:foundPsychatriast});
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:`Error: ${err}`});
+    }
+}
+
+module.exports = {retrieveMessagesBtwn, getAllPsychiatrist, retrieveUserChatPartners, sendMessage}
 // to send a message we require 
 /**
  *  - senderId this can be acquired from the token
