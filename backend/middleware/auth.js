7eed369f6097da53bf263575e1cc5862076c01d1
alsongard <alsongard@gmail.com>
2025-12-18 21:18:37 +0200

    Middleware:GetAuthenticated for security:Using cookies success

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,29 +1,54 @@
 const jwt = require("jsonwebtoken");
 const Student = require("../models/student.model");
+const Psychatriast = require("../models/physchatris.model");
 
 
 
-const getAuthenticated =  async(req, res)=>
+const getAuthenticated =  async(req, res, next)=>
 {
-    const authToken = res.cookie["authToken"];
+    console.log('entering getAuthenticated middleware');
+    const authToken = req.cookies.authToken;
 
     if (!authToken)
     {
         return res.status(400).json({success:false, msg:"Failed Authentication"})
     }
     const decodedToken  = jwt.verify(authToken, process.env.JWT_SECRET);
+    console.log('decodedToken');
+    console.log(decodedToken);
+    // decodedToken
+    // {
+        // userId: '6903a4963253494881272acb',
+        // role: 'student',
+        // iat: 1766052178,
+        // exp: 1766059378
+    // }
     if (!decodedToken)
     {
         return res.status(400).json({success:false, msg:"This is an invalid token"})
     }
     
-    const id = decodedToken.userId;
+    req.userId = decodedToken.userId;
+    req.role = decodedToken.role;
     // check if student exist
-    const foundStudent = await Student.findById({_id:id});
-
-    if(!foundStudent)
+    const role = decodedToken.role;
+    const id = decodedToken.userId;
+    if (role === "student") // this only runs when user not exist
     {
-        return res.status(400).json({success:false, msg:"Invalid token"})
+        const foundStudent = await Student.findById({_id:id});
+        if(!foundStudent)
+        {
+            return res.status(400).json({success:false, msg:"Invalid token"})
+        }
+    }
+    if (role === "psychiatrist")
+    {
+        // TO DO: Add psychiatrist model check here
+        const foundPsychiatrist  = await Psychatriast.findById({_id:id});
+        if(!foundPsychiatrist) // this only runs when user not exist
+        {
+            return res.status(400).json({success:false, msg:"Invalid token"})
+        }
     }
     // this means that the student has been found
     next(); // pass is decodedToken is safe
