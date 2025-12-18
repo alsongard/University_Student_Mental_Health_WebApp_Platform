31e5171b3a56525fdea744a83da577f5384dade5
alsongard <alsongard@gmail.com>
2025-12-18 21:29:45 +0200

    update on Foreign Key:Collection Name

--- backend/models/psychiatristSession.model.js
+++ backend/models/psychiatristSession.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const pyschiatristSession = new mongoose.Schema(
     {
-        psychiatristId: {type:mongoose.Schema.Types.ObjectId, ref:'Psychatriast', required:true},
+        psychiatristId: {type:mongoose.Schema.Types.ObjectId, ref:'Psychatriast', required:true},  // USING THIS ID YOU CAN GET PSYCH DETAILS OR PSYCHID INFO
         date:{type:Date , required:true},
         startTime: {type:String, required:true},
         endTime: {type:String, required:true},
