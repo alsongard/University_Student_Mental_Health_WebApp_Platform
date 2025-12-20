7ca7388f437c8d597ba3f510b1dc36b3cb784214
alsongard <alsongard@gmail.com>
2025-12-21 01:19:21 +0200

    added middlware to message controllers

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -2,11 +2,11 @@ const express = require("express");
 const {sendMessage, retrieveMessagesBtwn, retrieveUserChatPartners, getAllPsychiatrist} = require("../controllers/message.controller")
 const messageRouter = express.Router();
 
-
+const {getAuthenticated} = require("../middleware/auth")
 // getAllContacts from psychiatrist
 messageRouter.get("/getAllPsychiatrist", getAllPsychiatrist); //sidebar for all Contacts
 messageRouter.post("/sendmessage/:id", sendMessage);
-messageRouter.get("/retrievemessages/:id", retrieveMessagesBtwn);
-messageRouter.get("/retrieveUserChatPartners/:id", retrieveUserChatPartners);
+messageRouter.get("/retrievemessages/:id",getAuthenticated,  retrieveMessagesBtwn);
+messageRouter.get("/retrieveUserChatPartners", getAuthenticated, retrieveUserChatPartners);
 // messageRouter.get("/retrieveUsers/:id", getUsers);/
 module.exports = messageRouter;
