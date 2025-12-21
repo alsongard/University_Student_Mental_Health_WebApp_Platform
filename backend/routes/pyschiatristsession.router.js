73ef80e54d6eeca037ab6deaccd01fe198ccab69
alsongard <alsongard@gmail.com>
2025-12-21 22:25:25 +0200

    getAuthenticated middleware added

--- backend/routes/pyschiatristsession.router.js
+++ backend/routes/pyschiatristsession.router.js
@@ -1,12 +1,12 @@
 // view session info routers for Psychiatrist
 const express = require("express");
-
+const {getAuthenticated} = require("../middleware/auth")
 const pyschiatristSessionRouter = express.Router();
 const {UpdateSession, DeleteSession, createSession ,ViewPsychiatristSession} = require("../controllers/psySessionController")
 
 pyschiatristSessionRouter.post("/createSession",createSession);
 pyschiatristSessionRouter.put("/updateSession/:sessionId", UpdateSession);
-pyschiatristSessionRouter.get("/viewSession/:psychiatristId", ViewPsychiatristSession);
+pyschiatristSessionRouter.get("/viewSession", getAuthenticated, ViewPsychiatristSession);
 pyschiatristSessionRouter.delete("/deleteSession/:sessionId", DeleteSession);
 
 // pyschiatristSessionRouter.get("/getAllSessions", GetAllSessions); 
