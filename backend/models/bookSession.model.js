86584d21bfd4bee9d4c1745c394e90ad16362610
alsongard <alsongard@gmail.com>
2025-12-21 22:22:41 +0200

    bookSession Model updated for studentDetails reference

--- backend/models/bookSession.model.js
+++ backend/models/bookSession.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bookingSession = new mongoose.Schema({
     sessionId: {type:mongoose.Schema.Types.ObjectId, ref:'PsychiatristSession'},
     studentId: {type:mongoose.Schema.Types.ObjectId, ref:'Student'},
+    studentDetailsInfo: {type:mongoose.Schema.Types.ObjectId, ref:'StudentDetails'},
     psychiatristId: {type:mongoose.Schema.Types.ObjectId, ref:'Psychatriast'},
     status: {type:String, enum:[ 'scheduled', 'completed', 'cancelled'], default:'scheduled'}
 },
