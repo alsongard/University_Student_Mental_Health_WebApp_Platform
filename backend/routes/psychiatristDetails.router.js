8e9f4f6f6e7eb2e78bf85d1c3b07007d0e77a86a
alsongard <alsongard@gmail.com>
2025-12-21 22:24:47 +0200

    added getAuthenticated middleware for security

--- backend/routes/psychiatristDetails.router.js
+++ backend/routes/psychiatristDetails.router.js
@@ -1,11 +1,12 @@
 // this is psychiatrist details router
 const express = require("express");
 const psychiatristDetailsRouter = express.Router();
+const { getAuthenticated } = require("../middleware/auth");
 
 const { createPsychiatristDetails, getPsychiatristDetails, updatePsychiatristDetails} = require("../controllers/psychiatristDetails.controller");
 
-psychiatristDetailsRouter.post("/createPsychDetails", createPsychiatristDetails);
-psychiatristDetailsRouter.get("/getPsychiatristDetails/:id", getPsychiatristDetails);
-psychiatristDetailsRouter.put("/updatePsychiatristDetails/:id", updatePsychiatristDetails);
+psychiatristDetailsRouter.post("/createPsychDetails", getAuthenticated, createPsychiatristDetails);
+psychiatristDetailsRouter.get("/getPsychiatristDetails", getAuthenticated, getPsychiatristDetails);
+psychiatristDetailsRouter.put("/updatePsychiatristDetails", getAuthenticated, updatePsychiatristDetails);
 
 module.exports = psychiatristDetailsRouter;
