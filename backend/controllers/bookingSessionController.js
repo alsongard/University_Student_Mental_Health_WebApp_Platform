615ccb7f0eb74ce359d5444e1b37e2421ab4cdb2
alsongard <alsongard@gmail.com>
2026-01-09 00:27:06 +0200

    update on createBooking: mongosession for update maxBooking, getStudentPastSession and getFutureStudentSession for optimization

--- backend/controllers/bookingSessionController.js
+++ backend/controllers/bookingSessionController.js
@@ -4,6 +4,10 @@ const BookSession = require("../models/bookSession.model");
 const PsychiatristDetails = require("../models/psychiatristdetail.model");
 const Student = require("../models/student.model");
 const StudentDetails = require("../models/studentDetails.model");
+const mongoose = require("mongoose");
+const PsychiatristSession = require("../models/psychiatristSession.model");
+
+
 // create BookingSession
 module.exports.CreateBookingSession = async (req, res)=>
 {
@@ -21,11 +25,31 @@ module.exports.CreateBookingSession = async (req, res)=>
     {
         // console.log(`${sessionId}\n ${studentId}\n ${psychiatristId}\n ${status}`);
         return res.status(400).json({success:false, msg:"Invalid parameters"});
-    }
+    };
+    const session = mongoose.startSession();
+    session.startSession();
     try
     {
+        const foundPsychiatricSessions = await PsychiatristSession.findById({_id:sessionId});
+        if (foundPsychiatricSessions.maxBookings == 0)
+        {
+            return res.status(200).json({success:false,  msg:'The current session is fully booked', data:[]});
+        }
+
         const studentDetails = await StudentDetails.findOne({studentId: studentId})
-        const new_booking_session = await BookSession.create({sessionId:sessionId, studentId:studentId, studentDetailsInfo:studentDetails._id || "", psychiatristId:psychiatristId,  status:status});
+        const new_booking_session = new BookSession(
+            {
+                sessionId:sessionId,
+                studentId:studentId,
+                studentDetailsInfo:studentDetails._id || "",
+                psychiatristId:psychiatristId,
+                status:status
+            });
+            
+        await new_booking_session.save({session});
+        await PsychiatristSession.findByIdAndUpdate({_id:sessionId}, {$inc: {maxBookings: -1}}, {session});
+        await session.commitTransaction();
+        
         if (!new_booking_session)
         {
             return res.status(500).json({success:false, msg: 'Internal Server Error: booking Failed'})
@@ -186,6 +210,206 @@ module.exports.ViewStudentBookedSessions  = async (req,res)=>{
         // console.log("studentBookedSession");
         // console.log(studentBookedSession);
         return res.status(200).json({success:true, data:studentBookedSession} );
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:`Server Error: ${err}`});
+    }   
+};
+
+
+module.exports.getStudentPassSessions = async (req, res)=>
+{
+    console.log(`req.userId: ${req.userId}\n req.role: ${req.role}`)
+    const studentId = req.userId;
+    const role  = req.role;
+
+    if (role !== "student")
+    {
+        return res.status(403).json({success:false, msg:"Access denied: Only students can view their booked sessions"});
+    }
+
+    try
+    {
+        // check is student exist first
+        const foundStudentId = await Student.findById({_id:studentId});
+        if (!foundStudentId)
+        {
+            return res.status(404).json({success:false, msg:"Student not found"});
+        }
+        const foundStudentBookedSessions = await BookSession.find({studentId:studentId}).populate([{path: 'psychiatristId', select:"fullName specialization"}, {path: 'sessionId', select: 'sessionType date startTime endTime sessionMode sessionDuration sessionDuration'}] );
+        // console.log("foundStudentBookedSessions");
+        // console.log(foundStudentBookedSessions);
+        if (foundStudentBookedSessions.length == 0)
+        {
+            return res.status(200).json({success:true, msg:"You have no booked sessions"});
+        }
+        const pastStudentSessions =foundStudentBookedSessions.filter((sessionItem)=>{ return new Date(sessionItem.sessionId.date) < new Date()});
+        // console.log("pastStudentSessions");
+        // console.log(pastStudentSessions);
+        if (pastStudentSessions.length == 0)
+        {
+            return res.status(200).json({success:true, msg:"You have no past booked sessions"});
+        }
+        // console.log('found student booked sessions'); // after using cookies for auth
+        // console.log(foundStudentBookedSessions);
+        // get psychiatristId from booked sessions
+        const psychIds = [ ...new Set(pastStudentSessions.map((sessionInfo)=>{return sessionInfo.psychiatristId._id}))];
+
+        // console.log('psych Ids');
+        // console.log(psychIds); // I can confirm that i get an array of [ { _id: new ObjectId('692bbcb9946ace680fc7e177') } ]
+        // get psychiatrist details for each booked session
+        const psychDetails = await PsychiatristDetails.find({psychiatristId:{$in:psychIds}}).select('fullName psychiatristId specilization');
+        // console.log('psychDetails');
+        // console.log(psychDetails);
+        /*
+        [
+            {
+                _id: new ObjectId('6939c45d446162c08bb9b3da'),
+                psychiatristId: new ObjectId('692bbcb9946ace680fc7e177'),
+                fullName: 'Dr. Michael Chen',
+                specilization: 'Depression & Anxiety'
+            }
+        ]
+        */
+        const psychDetailsMap = {}
+
+        psychDetails.forEach((detail)=>{
+            // console.log('detail.psychiatristId.toString()');
+            // console.log(detail.psychiatristId.toString());
+            // console.log('detail.fullName');
+            // console.log(detail.fullName);
+            // console.log('detail.specilization');
+            // console.log(detail.specilization);
+
+            psychDetailsMap[detail.psychiatristId.toString()]  = {
+                fullName: detail.fullName,
+                specialization:detail.specilization
+            }
+        })
+
+        // console.log('psychDetailsMap');
+        // console.log(psychDetailsMap);
+
+        // to return booked session
+        const studentBookedSession = pastStudentSessions.map((session)=>{
+            // get DETAILS based on psychDetails Id
+            // console.log('session.psychiatristId._id');
+            // console.log(session.psychiatristId._id);
+
+            // console.log('session.psychiatristId._id.toString()');
+            // console.log(session.psychiatristId._id.toString());
+
+            const detail = psychDetailsMap[session.psychiatristId._id.toString()] || {}; // this will return the given values:fullName,specilization based on psychId
+            return {
+                ...session.toObject(),
+                fullName: detail.fullName,
+                specilization: detail.specialization
+            }
+        });
+        // console.log("studentBookedSession");
+        // console.log(studentBookedSession);
+        return res.status(200).json({success:true, msg:"past student sessions", data:studentBookedSession} );
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:`Server Error: ${err}`});
+    }   
+}
+module.exports.getFutureStudentSessions = async (req, res)=>
+{
+    console.log(`req.userId: ${req.userId}\n req.role: ${req.role}`)
+    const studentId = req.userId;
+    const role  = req.role;
+
+    if (role !== "student")
+    {
+        return res.status(403).json({success:false, msg:"Access denied: Only students can view their booked sessions"});
+    }
+
+    try
+    {
+        // check is student exist first
+        const foundStudentId = await Student.findById({_id:studentId});
+        if (!foundStudentId)
+        {
+            return res.status(404).json({success:false, msg:"Student not found"});
+        }
+        const foundStudentBookedSessions = await BookSession.find({studentId:studentId}).populate([{path: 'psychiatristId', select:"fullName specialization"}, {path: 'sessionId', select: 'sessionType date startTime endTime sessionMode sessionDuration sessionDuration'}] );
+        // console.log("foundStudentBookedSessions");
+        // console.log(foundStudentBookedSessions);
+        if (foundStudentBookedSessions.length == 0)
+        {
+            return res.status(200).json({success:true, msg:"You have no booked sessions"});
+        }
+        const futureStudentSessions =foundStudentBookedSessions.filter((sessionItem)=>{ return new Date(sessionItem.sessionId.date) > new Date()});
+        // console.log("futureStudentSessions");
+        // console.log(futureStudentSessions);
+        if (futureStudentSessions.length == 0)
+        {
+            return res.status(200).json({success:true, msg:"You have no future booked sessions"});
+        }
+        // console.log('found student booked sessions'); // after using cookies for auth
+        // console.log(foundStudentBookedSessions);
+        // get psychiatristId from booked sessions
+        const psychIds = [ ...new Set(futureStudentSessions.map((sessionInfo)=>{return sessionInfo.psychiatristId._id}))];
+
+        // console.log('psych Ids');
+        // console.log(psychIds); // I can confirm that i get an array of [ { _id: new ObjectId('692bbcb9946ace680fc7e177') } ]
+        // get psychiatrist details for each booked session
+        const psychDetails = await PsychiatristDetails.find({psychiatristId:{$in:psychIds}}).select('fullName psychiatristId specilization');
+        // console.log('psychDetails');
+        // console.log(psychDetails);
+        /*
+        [
+            {
+                _id: new ObjectId('6939c45d446162c08bb9b3da'),
+                psychiatristId: new ObjectId('692bbcb9946ace680fc7e177'),
+                fullName: 'Dr. Michael Chen',
+                specilization: 'Depression & Anxiety'
+            }
+        ]
+        */
+        const psychDetailsMap = {}
+
+        psychDetails.forEach((detail)=>{
+            // console.log('detail.psychiatristId.toString()');
+            // console.log(detail.psychiatristId.toString());
+            // console.log('detail.fullName');
+            // console.log(detail.fullName);
+            // console.log('detail.specilization');
+            // console.log(detail.specilization);
+
+            psychDetailsMap[detail.psychiatristId.toString()]  = {
+                fullName: detail.fullName,
+                specialization:detail.specilization
+            }
+        })
+
+        // console.log('psychDetailsMap');
+        // console.log(psychDetailsMap);
+
+        // to return booked session
+        const studentBookedSession = futureStudentSessions.map((session)=>{
+            // get DETAILS based on psychDetails Id
+            // console.log('session.psychiatristId._id');
+            // console.log(session.psychiatristId._id);
+
+            // console.log('session.psychiatristId._id.toString()');
+            // console.log(session.psychiatristId._id.toString());
+
+            const detail = psychDetailsMap[session.psychiatristId._id.toString()] || {}; // this will return the given values:fullName,specilization based on psychId
+            return {
+                ...session.toObject(),
+                fullName: detail.fullName,
+                specilization: detail.specialization
+            }
+        });
+        // console.log("studentBookedSession");
+        // console.log(studentBookedSession);
+        return res.status(200).json({success:true, msg:"future student sessions", data:studentBookedSession} );
     }
     catch(err)
     {
